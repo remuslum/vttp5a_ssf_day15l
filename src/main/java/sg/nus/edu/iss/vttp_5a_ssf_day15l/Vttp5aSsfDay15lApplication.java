b00b3f9595105e51042973b28141f3580e017b20
remuslum <57741494+remuslum@users.noreply.github.com>
2024-11-22 11:04:24 +0800

    First Commit

---
+++ src/main/java/sg/nus/edu/iss/vttp_5a_ssf_day15l/Vttp5aSsfDay15lApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp_5a_ssf_day15l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfDay15lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfDay15lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/edu/iss/vttp_5a_ssf_day15l/Vttp5aSsfDay15lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp_5a_ssf_day15l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay15lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
